84e9df601b36f1cbb6aaaf8c18b95696b005bfc4
mohammadallasasmeh90-design <3.07586671e+08+mohammadallasasmeh90-design@users.noreply.github.com>
2026-09-16 17:16:29 +0000

    Bust PWA cache for latest SmartMind features

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='smartmind-v2';
+const CACHE='smartmind-v4';
 const ASSETS=['./','./index.html','./manifest.json','./privacy.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
